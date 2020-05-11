a409b71f07285e25d2ee58ea08538dcaad792247
Lalit sekhar Behera <lalitsekhar1999@gmail.com>
2020-05-11 19:58:58 +0530

    added custom scrollbar

--- todo.js
+++ todo.js
@@ -3,13 +3,17 @@ const input = document.querySelector('#input');
 const buttonn = document.querySelector('#button');
 
 const addlist = () => {
-    let todo = input.value;
-    // console.log(todo);
-    let li = document.createElement('li');
-    let text = document.createTextNode(`${todo}`);
-    li.appendChild(text);
-    ullist.appendChild(li);
-    input.value = '';
+    if (input.value !== '') {
+        let todo = input.value;
+        // console.log(todo);
+        let li = document.createElement('li');
+        let text = document.createTextNode(`${todo}`);
+        li.appendChild(text);
+        ullist.appendChild(li);
+        input.value = '';
+    } else {
+        //TODO:  add warning;
+    }
 }
 
 buttonn.addEventListener('click', addlist);
