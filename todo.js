8eb9ad6de6e93ffb32d003854e42c213cdf8c67f
Lalit sekhar Behera <lalitsekhar1999@gmail.com>
2020-05-13 20:01:57 +0530

    added delete icon

--- todo.js
+++ todo.js
@@ -1,6 +1,8 @@
 const ullist = document.querySelector('#ullist');
 const input = document.querySelector('#input');
 const buttonn = document.querySelector('#button');
+const deleteimg = document.querySelector('#deleteimg');
+const list = document.querySelector('li');
 
 const addlist = () => {
     if (input.value !== '') {
@@ -8,7 +10,20 @@ const addlist = () => {
         // console.log(todo);
         let li = document.createElement('li');
         let text = document.createTextNode(`${todo}`);
-        li.appendChild(text);
+        let divOne = document.createElement('div');
+        let divTwo = document.createElement('div');
+        let img = document.createElement('img');
+
+        img.src = "interface.png";
+        img.setAttribute("id", "deleteimg");
+
+        divOne.classList.add('divOne');
+        divTwo.classList.add('divTwo');
+
+        divOne.appendChild(text);
+        divTwo.appendChild(img);
+        li.appendChild(divOne);
+        li.appendChild(divTwo);
         ullist.appendChild(li);
         input.value = '';
     } else {
@@ -16,5 +31,12 @@ const addlist = () => {
     }
 }
 
+const deletelist = () => {
+    li.style.display = 'none';
+    console.log('delete');
+    
+}
+
 buttonn.addEventListener('click', addlist);
+deleteimg.addEventListener('click', deletelist);
 
